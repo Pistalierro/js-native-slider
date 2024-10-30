b12c5489c682e8fc4384c10f315701406c77abce
Anatoliy Sysoienko <pistalierro@gmail.com>
2024-10-30 14:34:48 +0200

    статическая разметка + черновое оформление + логика прокручивания слайдера -> вперед, назад, постановка на паузу

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,60 @@
-console.log('main.js loaded 1');
-console.log('main.js loaded 2');
-console.log('main.js loaded 3');
-console.log('main.js loaded 4');
-console.log('main.js loaded 5');
+const slides = document.querySelectorAll('.slide');
+const pauseBtn = document.querySelector('#pause');
+const prevBtn = document.querySelector('#prev');
+const nextBtn = document.querySelector('#next');
 
-const str = 'some string!!!';
-console.log(str);
+let currentSlide = 0;
+let isPlaying = true;
+let timerID = null;
+let interval = 1000;
 
-function func() {
-  console.log('Function is work');
+const SLIDES_LENGTH = slides.length;
+
+const _tick = () => timerID = setInterval(goToNext, interval);
+
+function goToNth(n) {
+  slides[currentSlide].classList.toggle('active');
+  currentSlide = (n + SLIDES_LENGTH) % SLIDES_LENGTH;
+  slides[currentSlide].classList.toggle('active');
 }
 
-func();
+function goToPrev(n) {
+  goToNth(currentSlide - 1);
+}
+
+function goToNext(n) {
+  goToNth(currentSlide + 1);
+}
+
+function prev() {
+  goToPrev();
+  pause();
+}
+
+function next() {
+  goToNext();
+  pause();
+}
+
+function pause() {
+  clearInterval(timerID);
+  isPlaying = !isPlaying;
+  pauseBtn.innerHTML = 'Play';
+
+}
+
+function play() {
+  clearInterval(timerID);
+  _tick();
+  isPlaying = !isPlaying;
+  pauseBtn.innerHTML = 'Pause';
+}
+
+const pausePlay = () => isPlaying ? pause() : play();
+
+pauseBtn.addEventListener('click', pausePlay);
+prevBtn.addEventListener('click', prev);
+nextBtn.addEventListener('click', next);
+
+_tick();
 
