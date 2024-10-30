d39a9406bca94b61a091877ddbc8e36c11d8b552
Anatoliy Sysoienko <pistalierro@gmail.com>
2024-10-30 17:36:16 +0200

    логика управления свайпами

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,22 @@
-const slides = document.querySelectorAll('.slide');
-const indicators = document.querySelectorAll('.indicator');
-const pauseBtn = document.querySelector('#pause');
-const prevBtn = document.querySelector('#prev');
-const nextBtn = document.querySelector('#next');
+const container = document.querySelector('#carousel');
+const slides = container.querySelectorAll('.slide');
+const indicatorsContainer = container.querySelector('.indicators');
+const indicators = indicatorsContainer.querySelectorAll('.indicator');
+const pauseBtn = container.querySelector('#pause');
+const prevBtn = container.querySelector('#prev');
+const nextBtn = container.querySelector('#next');
 
 let currentSlide = 0;
 let isPlaying = true;
 let timerID = null;
 let interval = 1000;
+let touchStart = null;
+let touchEnd = null;
 
 const SLIDES_LENGTH = slides.length;
-
-const _tick = () => timerID = setInterval(goToNext, interval);
+const ARROW_LEFT = 'ArrowLeft';
+const ARROW_RIGHT = 'ArrowRight';
+const SPACE = 'Space';
 
 function goToNth(n) {
   slides[currentSlide].classList.toggle('active');
@@ -28,6 +33,8 @@ function goToPrev() {
 function goToNext() {
   goToNth(currentSlide + 1);
 }
+
+const _tick = () => timerID = setInterval(goToNext, interval);
 
 function prev() {
   goToPrev();
@@ -55,15 +62,37 @@ function play() {
 
 const pausePlay = () => isPlaying ? pause() : play();
 
-function indicate() {
-  goToNth(+this.getAttribute('data-slide-to'));
-  pause();
+function indicate(e) {
+  const target = e.target;
+  if (target && target.classList.contains('indicator')) {
+    goToNth(+target.dataset.slideTo);
+    pause();
+  }
+}
+
+function pressKey(e) {
+  if (e.code === SPACE) pausePlay();
+  if (e.code === ARROW_LEFT) prev();
+  if (e.code === ARROW_RIGHT) next();
+}
+
+function swipeStart(e) {
+  touchStart = e.changedTouches[0].clientX;
+}
+
+function swipeEnd(e) {
+  touchEnd = e.changedTouches[0].clientX;
+  if (touchStart < touchEnd && touchStart - touchEnd < -100) prev();
+  if (touchStart > touchEnd && touchStart - touchEnd > 100) next();
 }
 
 pauseBtn.addEventListener('click', pausePlay);
 prevBtn.addEventListener('click', prev);
 nextBtn.addEventListener('click', next);
+indicatorsContainer.addEventListener('click', indicate);
+container.addEventListener('touchstart', swipeStart);
+container.addEventListener('touchend', swipeEnd);
+document.addEventListener('keydown', pressKey);
 
 _tick();
 
-indicators.forEach(indicator => indicator.addEventListener('click', indicate));
